f2a4fb0161eab3d623210b8e0dcc6b424eef8296
Zachary Donsky <zachary.donsky@gmail.com>
2018-06-06 15:57:28 -0400

    fixed a bunch of stuff

--- js/index.js
+++ js/index.js
@@ -19,24 +19,3 @@
     nodeMenuButton: nodeMenu.querySelector('.js-menu__toggle')
   });
 })();
-
-///////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////
-// The highlight text //
-
-/**
-  Javascript isn't required for this effect,
-  I just added a quick piece of jQuery code
-  to simulate a hover for the animated preview
-  on CodePen.io.
-*/
-
-// simulation code, ignore it.
-// $(function() {
-//   setTimeout(function() {
-//     $("article.highlight").toggleClass("hover");
-//     setTimeout(function() {
-//       $("article.highlight").toggleClass("hover");
-//     }, 2000);
-//   }, 1000);
-// });
